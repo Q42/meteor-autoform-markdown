0530e6b021c7afc9e99b0c4065e7de615399ef57
Jeroen Gijsman <jeroen@q42.nl>
2015-04-22 22:11:04 +0200

    added version numbers to packages

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
   api.use(['templating', 'ui', 'blaze'], 'client');
-  api.use(['perak:markdown', 'aldeed:autoform'], 'client');
+  api.use(['perak:markdown@1.0.5', 'aldeed:autoform@4.2.2'], 'client');
   api.addFiles([
     'markdown.html',
     'markdown.css',
