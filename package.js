16e4bd9b036a075d3c9df7b1f25d622cce10b17e
Jeroen Gijsman <jeroen@q42.nl>
2015-08-28 11:44:27 +0200

    removend unnecessary deps version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:autoform-markdown',
-  version: '0.2.0',
+  version: '0.2.5',
   summary: 'A simple autoform markdown with preview using perak:markdown',
   git: 'https://github.com/Q42/meteor-autoform-markdown',
   documentation: 'README.md'
