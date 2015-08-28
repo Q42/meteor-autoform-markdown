8116266f682c9279ee777d30496b2961e10d41e6
Jeroen Gijsman <jeroen@q42.nl>
2015-08-28 11:39:27 +0200

    version bump for autoform

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:autoform-markdown',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'A simple autoform markdown with preview using perak:markdown',
   git: 'https://github.com/Q42/meteor-autoform-markdown',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
   api.use(['templating', 'ui', 'blaze'], 'client');
-  api.use(['perak:markdown@1.0.5', 'aldeed:autoform@5.3.0'], 'client');
+  api.use(['perak:markdown@1.0.5', 'aldeed:autoform@5.4.1'], 'client');
   api.addFiles([
     'markdown.html',
     'markdown.css',
